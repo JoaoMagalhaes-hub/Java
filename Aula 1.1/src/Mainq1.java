a3cfea67f5f3b18caa2b2ec5f47ea52f20e33f89
JoaoMagalhaes-hub <142951817+JoaoMagalhaes-hub@users.noreply.github.com>
2024-02-05 21:15:25 -0300

    Add files via upload

---
+++ Aula 1.1/src/Mainq1.java
@@ -0,0 +1,11 @@
+public class Mainq1 {
+    public static void main(String[] args) {
+
+        int hor1 = 15;
+        int tothor1 = 40;
+        int total;
+
+        total = hor1 * tothor1;
+            System.out.println(total);
+        }
+    }
---
+++ Aula 1.1/src/Mainq4.java
@@ -0,0 +1,22 @@
+public class Mainq4 {
+    public static void main(String[] args) {
+
+        double salario1 = 1800 ;
+
+        if (salario1 <= 900){
+            System.out.println(" Insento ");
+        } else if (salario1 <= 1500){
+            System.out.println(" Você tem desconto de 5%");
+        } else if (salario1 <= 2500){
+            System.out.println(" Você tem desconto 10%");
+        } else {
+            System.out.println((" Você tem desconto de 20%"));
+        }
+
+
+
+
+
+
+    }
+}
