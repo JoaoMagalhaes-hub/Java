1f7bc9aab42fca303fae788879aa7eafce9a610f
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2024-02-19 19:46:06 -0300

    Raiva

---
+++ Aula 2/Aula_2.java
@@ -0,0 +1,21 @@
+package Aula 2.src;
+    public class Aula_2 {
+        public static void main(String[] args){
+    
+            int x= 1;
+            switch(x){
+                case 0:
+                System.out.println("Opção 0 escolhida!");
+                break;
+                case 1:
+                System.out.println("Opção 1 escolhida!");
+                break;
+                case 2:
+                System.out.println("Opção 2 escolhida!");
+                break;
+                default:
+                System.out.println("Opção inválida!");
+            }
+        }
+    }
+
