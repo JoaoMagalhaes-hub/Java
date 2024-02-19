403686e676ff4ddb42ff284ca884fc12f79d7dd2
JoaoMagalhaes-hub <142951817+JoaoMagalhaes-hub@users.noreply.github.com>
2024-02-19 19:53:01 -0300

    Update Aula_2.java

--- Aula 2/Aula_2.java
+++ Aula 2/Aula_2.java
@@ -1,21 +1,25 @@
-package Aula 2.src;
-    public class Aula_2 {
-        public static void main(String[] args){
-    
-            int x= 1;
-            switch(x){
-                case 0:
+package Aula_2.src;
+public class Aula_2 {
+    public static void main(String[] args){
+
+        int x= 1;
+        int repetir = 0;
+        do{
+            System.out.println("Caso escolha...");
+        switch(x) {
+            case 0:
                 System.out.println("Opção 0 escolhida!");
                 break;
-                case 1:
+            case 1:
                 System.out.println("Opção 1 escolhida!");
                 break;
-                case 2:
+            case 2:
                 System.out.println("Opção 2 escolhida!");
                 break;
-                default:
+            default:
                 System.out.println("Opção inválida!");
             }
-        }
+        }while(repetir==0);
     }
+}
 
