b8c8a7055bc925099df01ca251bb1b1925923754
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2024-03-18 20:59:23 -0300

    Exercício não finalizado

---
+++ Aula_6/Ex_2/Animal.java
@@ -0,0 +1,10 @@
+package Aula_6.Ex_2;
+
+public class Animal {
+
+    private String especie;
+    private String caraceteristica;
+    private String descricao;
+
+public class Leao extends Animal'''''
+}
--- Aula_6/Ex_2/Main.java
+++ Aula_6/Ex_2/Main.java
@@ -1,8 +1,20 @@
 package Aula_6.Ex_2;
 
 public class Main {
-    public static void main(String[] args){
+    public static main(String[] args){
 
-        Leao leao = 
+        Leao leao = new Leao ("Mamífero", "Patas". "Juba");
+        Arara arara = new Aarara();
+
+        System.out.println("Informações do Leão: " );
+        //Exibir as informações do mamífero
+
+        System.out.println("Informações da Arara: ");
+        System.out.println(arara.getClasse());
+        System.out.println(arara.getLocomocao());
+        System.out.println(arara.getQtdeOvos());
+
+
+
     }
 }
