7deb6d36a8b50d608eee6d0f81fbfd7bd04edea2
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2024-04-22 20:01:54 -0300

    Identando

--- Aula_8/Questão_1/Main.java
+++ Aula_8/Questão_1/Main.java
@@ -21,8 +21,33 @@ public class Main {
             p.Voou();
             System.out.print("Animal: Sapo, Nome: " + s.nome + " , Idade: " + s.idade + " , Status: ");
             s.Correr();
-            System.out.print("Animal: Pássaro, Nome: " + pe.nome + " , Idade: " + pe.idade + " , Status: ");    
+            System.out.print("Animal: Peixe Nome: " + pe.nome + " , Idade: " + pe.idade + " , Status: ");    
             pe.Nadou();
+
+            Animal a1 = new Cachorro("Billy", 13 , TipoAnimal.MAMIFERO);
+            a1.emitirSom();
+            a1.emitirComer();
+            ((Cachorro)a1).Correr();
+
+            Animal a2 = new Gato("Mila", 8,  TipoAnimal.MAMIFERO);
+            a2.emitirSom();
+            a2.emitirComer();
+            ((Gato)a2).Correr();
+
+            Animal a3 = new Passarinho("Adamastor", 10, TipoAnimal.AVE);
+            a3.emitirSom();
+            a3.emitirComer();
+            ((Passarinho)a3).Voou();
+
+            Animal a4 = new Sapo("Crazy frog", 97, TipoAnimal.ANFIBIO);
+            a4.emitirSom();
+            a4.emitirComer();
+            ((Sapo)a4).Correr();
+
+            Animal a5 = new Peixe("Pexe", 200, TipoAnimal.PEIXE);
+            a5.emitirSom();
+            a5.emitirComer();
+            ((Peixe)a5).Nadou();
 
     }
 
